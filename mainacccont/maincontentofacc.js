d5f627c2c4b9a857224b512caf985a7d1986ce69
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-21 16:27:48 +0300

    upd

--- mainacccont/maincontentofacc.js
+++ mainacccont/maincontentofacc.js
@@ -4,10 +4,15 @@ export default function MainContentOfAcc() {
   return (
     <div className="container-fluid" style={MainContentOfAccCSS}>
       <div className="aibf2u container-fluid d-flex">
-        <div className="accout-pfp">
-          <img src="#" className="profile-picture bg-danger"></img>
+        <div className="accout-pfp text-center w-100 bg-success">
+          <img
+            src="https://imj31us4am50.github.io/imgs/feline-bw.jpeg"
+            className="profile-picture"
+          ></img>
         </div>
-        <div className="account-information"></div>
+        <div className="account-information text-center w-100 bg-danger">
+          annaoop
+        </div>
       </div>
     </div>
   );
