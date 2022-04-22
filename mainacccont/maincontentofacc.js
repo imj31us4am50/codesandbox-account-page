4ae376a8c3c140e3e9357b2aa05ee6c80712c8cc
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-22 16:35:27 +0300

    upd

--- mainacccont/maincontentofacc.js
+++ mainacccont/maincontentofacc.js
@@ -3,15 +3,51 @@ import MainContentOfAccCSS from "/mainacccont/maincontentofacc.css";
 export default function MainContentOfAcc() {
   return (
     <div className="container-fluid" style={MainContentOfAccCSS}>
-      <div className="aibf2u container-fluid d-flex">
-        <div className="accout-pfp text-center w-100 bg-success">
+      <div className="aibf2u container-fluid d-flex align-items-start">
+        <div className="accout-pfp text-center w-100 ">
           <img
             src="https://imj31us4am50.github.io/imgs/feline-bw.jpeg"
             className="profile-picture"
           ></img>
+          <div className="tltp">
+            <img
+              src="https://imj31us4am50.github.io/imgs/edit-pencil-icon.png"
+              className="edit-pencil-icon"
+            />
+            <span className="tltptext">Schimbă imaginea de profil</span>
+          </div>
         </div>
-        <div className="account-information text-center w-100 bg-danger">
-          annaoop
+        <div className="account-information h4 w-100 mt-4">
+          {/* looks bad
+          <div className="d-flex container-of-infoacc align-items-center">
+            <div className="thediv">Nume utilizator:</div>
+            <span id="username" className="thespan ml-2">
+              naturelover271
+            </span>
+          </div>
+          <br />
+          <div className="d-flex container-of-infoacc align-items-center">
+            <div className="thediv">Parolă:</div>
+            <span id="password" className="thespan ml-2"></span>
+          </div> */}
+          <div className="info1 p-2">
+            <div className="username-require d-inline">Nume utilizator: </div>
+            <input
+              type="text"
+              readOnly
+              value="naturelover271"
+              id="username-add"
+            />
+          </div>
+          <div className="info2 p-2">
+            <div className="password-require d-inline">Parolă: </div>
+            <input
+              type="password"
+              readOnly
+              value="myPassword123"
+              id="password-add"
+            />
+          </div>
         </div>
       </div>
     </div>
